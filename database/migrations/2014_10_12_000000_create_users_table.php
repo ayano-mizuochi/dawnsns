b5ea0e9ccdd0ad814edfdc1efc6fb5e65accdcb8
ayano-mizuochi <akira.haine0117@gmail.com>
2024-01-26 14:42:42 +0000

    detabase修正

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->string('name', 225);
             $table->string('email', 225)->unique();
             $table->string('password', 225);
-            $table->string('bio', 225);
-            $table->string('image', 225);
+            $table->string('bio', 225)->nullable();
+            $table->string('image', 225)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
